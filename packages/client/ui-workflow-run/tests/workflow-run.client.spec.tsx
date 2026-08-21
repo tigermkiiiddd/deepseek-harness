9ddf49f72a94497ed09995545df9b9afaeac2f5b
DESKTOP-V8INDAT\shinobi <z87213@gmail.com>
2026-08-22 03:34:18 +0800

    packages(client/ui-workflow-run): workflow run test

--- packages/client/ui-workflow-run/tests/workflow-run.client.spec.tsx
+++ packages/client/ui-workflow-run/tests/workflow-run.client.spec.tsx
@@ -293,7 +293,7 @@ function panelProps(data: WorkflowRunChatData, sessions = listState(), openSessi
     openFile: () => {},
     inspectCall: () => {},
     forkAt: () => {},
-    resendUserMessage: () => {},
+    rerunUserMessage: () => {},
     loadImage: () => Promise.reject(new Error('unused')),
     fileMentions: () => undefined,
     openSession,
